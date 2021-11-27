25dc96696ba81cd1dbe6d7fb12567e1e9b77f493
92257852915 <artur.santos@serpro.gov.br>
2021-11-27 06:20:16 -0300

    Básico em orientação a objeto

---
+++ src/a2bCriandoClasseseObjetosEmJava/CriandoClassesEObjetosEmJava.java
@@ -0,0 +1,13 @@
+package a2bCriandoClasseseObjetosEmJava;
+
+/**
+ * 0. O que é um objeto,classe,atributo, método, estado,abstração.
+ * 1. O que é um objeto?
+ * 2. Como definimos uma classe?
+ * 3. Instanciamos objetos?
+ * 4. Como manipula seus atributos e seus métodos?
+ * 5.
+ */
+
+public class CriandoClassesEObjetosEmJava {
+}
