80089aa399baeef703183145b01a6aafd0604baf
92257852915 <artur.santos@serpro.gov.br>
2021-11-27 06:46:52 -0300

    Definição do algoritmo que será aplicado.

--- src/a2bCriandoClasseseObjetosEmJava/CriandoClassesEObjetosEmJava.java
+++ src/a2bCriandoClasseseObjetosEmJava/CriandoClassesEObjetosEmJava.java
@@ -2,11 +2,41 @@ package a2bCriandoClasseseObjetosEmJava;
 
 /**
  * 0. O que é um objeto,classe,atributo, método, estado,abstração.
+ * ---------------------------------------------------------------------------------
  * 1. O que é um objeto?
+ *      Resp: 1.1- Objeto é uma coisa material ou abstrata.
+ *            1.2- Que pode ser percebida pelos sentidos.
+ *            1.3- E descrita por meio das caracteristicas = atributos.
+ *            1.4- Comportamentos = métodos
+ *            1.5- E estado atual = status.
+ *   --------------------------------------------------------------------------------
  * 2. Como definimos uma classe?
  * 3. Instanciamos objetos?
  * 4. Como manipula seus atributos e seus métodos?
- * 5.
+ * ----------------------------------------------------------------------------------
+ *
+ * 
+ * ***************ALGORITMO**********************
+ *                                              *
+ *        Classe Caneta                         *
+ *              modelo: Caractere -> String     *
+ *              cor: Caractere -> String        *
+ *              ponta: Real -> double           *
+ *              carga: inteiro -> int           *
+ *              tampada: Logico -> boolean      *
+ *          METODO rabiscar()                   *
+ *              Se(tampada) então               *
+ *                  Escreva("ERRO")             *
+ *              senao                           *
+ *                  Escreva ("Rabisco")         *
+ *              FimSe                           *
+ *          FIMMETODO                           *
+ *          Metodo tampar()                     *
+ *              tampada = verdadeiro            *
+ *          FimMetodo                           *
+ *        FimClasse                             *
+ *                                              *
+ * **********************************************
  */
 
 public class CriandoClassesEObjetosEmJava {
