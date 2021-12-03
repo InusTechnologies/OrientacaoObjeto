e504d4932bd20f0b33274932d3cd432333f8417e
92257852915 <artur.santos@serpro.gov.br>
2021-12-03 15:10:25 -0300

    Conclusão projeto!

---
+++ src/a4bmetodosEspeciais/Caneta.java
@@ -0,0 +1,40 @@
+       /* -----------------------
+        * |       CANETA        |                       |
+        * |---------------------|
+        * |     + modelo        |
+        * |     - ponta         |
+        * |                     |
+        * |---------------------|
+        * |      + getModelo()  |
+        * |      + setModelo()  |
+        * |      + getPonta()   |
+        * |      - setPonta()   |
+        *  ----------------------
+        *
+        * Classe Caneta
+        *   publico modelo:Caractere
+        *   privado ponta: Real
+        *   publico Metodo getModelo()
+        *       retorne modelo
+        *   FimMetodo
+        *   publico Metodo setModelo
+        *           (m:caractere)
+        *       modelo = m
+        *   FimModelo
+        *   publico Metodo getPonta()
+        *       retorne ponta
+        *   FimMetodo
+        *   publico Metodo setPonta
+        *           (p:Real)
+        *       ponta = p
+        *   FimMetodo
+        * FimClasse
+        */
+
+package a4bmetodosEspeciais;
+
+public class Caneta {
+    String modelo;
+    float ponta;
+
+}
--- src/a4bmetodosEspeciais/MetodosGetterSetterConstrutor.java
+++ src/a4bmetodosEspeciais/MetodosGetterSetterConstrutor.java
@@ -1,4 +1,7 @@
 package a4bmetodosEspeciais;
 
 public class MetodosGetterSetterConstrutor {
+    public static void main(String[] args) {
+
+    }
 }
