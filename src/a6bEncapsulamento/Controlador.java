40ef63f6053d45d2d408000bdc51832ab8393fce
92257852915 <artur.santos@serpro.gov.br>
2021-12-04 16:35:49 -0300

    UML interface

---
+++ src/a6bEncapsulamento/Controlador.java
@@ -0,0 +1,22 @@
+/*
+
+//interface Controlador
+////Métodos abstratos
+//        publico abstrato Metodo ligar()
+//        publico abstrato Metodo desligar()
+//        publico abstrato Metodo abrirMenu()
+//        publico abstrato Metodo fecharMenu()
+//        publico abstrato Metodo maisVolume()
+//        publico abstrato Metodo menosVolume()
+//        publico abstrato Metodo ligarMudo()
+//        publico abstrato Metodo desligarMudo()
+//        publico abstrato Metodo play()
+//        publico abstrato Metodo pause()
+//fimInterface
+        
+*/
+
+
+package a6bEncapsulamento;
+public interface Controlador {
+}
