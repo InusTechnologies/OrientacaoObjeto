52ae056c14256ce0c38d47d4a4b5befc07e51fb6
92257852915 <artur.santos@serpro.gov.br>
2021-12-04 17:29:37 -0300

    Implementando play  e pause e concluimos a declaração da classe

--- src/a6bEncapsulamento/ControleRemoto.java
+++ src/a6bEncapsulamento/ControleRemoto.java
@@ -139,11 +139,16 @@ public class ControleRemoto implements Controlador {
 
     @Override
     public void play() {
+        if (this.getLigado() && ! (this.getTocando())){
+            this.setTocando(true);
+        }
 
     }
 
     @Override
     public void pause() {
-
+        if (this.getLigado() &&  (this.getTocando())){
+            this.setTocando(false);
+        }
     }
 }
