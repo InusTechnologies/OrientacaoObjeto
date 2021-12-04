f79c3ea9e03aa62f5b169e3013ba9def2e1b6312
92257852915 <artur.santos@serpro.gov.br>
2021-12-04 16:45:58 -0300

    Criando uma lista de métodos públicos abstratos na interface

--- src/a6bEncapsulamento/Controlador.java
+++ src/a6bEncapsulamento/Controlador.java
@@ -13,10 +13,21 @@
 //        publico abstrato Metodo play()
 //        publico abstrato Metodo pause()
 //fimInterface
-        
+
 */
 
 
 package a6bEncapsulamento;
 public interface Controlador {
+    public abstract void ligar(); //NEM SEMPRE É VOID PODE TER UM TIPO DE RETORNO
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
+
 }
