18af8874f599d9911ec5c83364f8b446b1e26e26
92257852915 <artur.santos@serpro.gov.br>
2021-12-05 13:45:08 -0300

    criaçaõ getters e setters

--- src/a7ObjetosCompostos/Luta.java
+++ src/a7ObjetosCompostos/Luta.java
@@ -34,4 +34,38 @@ public class Luta {
     public void Lutar(){
 
     }
+
+    //Metodos especiais
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
 }
