b456e6b7cf9134163518d81d30d2347eb009555e
92257852915 <artur.santos@serpro.gov.br>
2021-12-05 17:02:47 -0300

    getters e setters

---
+++ src/a9Exercicio/Livro.java
@@ -0,0 +1,46 @@
+package a9Exercicio;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+    public void detalhes(){
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+
+}
---
+++ src/a9Exercicio/Publicacao.java
@@ -0,0 +1,11 @@
+package a9Exercicio;
+
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPag();
+    public void voltarPag(){
+
+    }
+}
