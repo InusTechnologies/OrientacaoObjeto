7b59e5de5208b3878608be53651b8d02cfe1632b
92257852915 <artur.santos@serpro.gov.br>
2021-12-05 19:35:14 -0300

    11 Criação da Classe principal e pessoa

---
+++ src/a11Herança2/Pessoa11.java
@@ -0,0 +1,12 @@
+package a11Herança2;
+
+public abstract class Pessoa11 {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public void fazerAniversario(){
+
+    }
+}
+
---
+++ src/a11Herança2/Principal11.java
@@ -0,0 +1,23 @@
+/*
+ *SuperClasse
+ * Sub-classe
+ * Ancestral
+ * Descendente
+ * Classe Raiz
+ * Classe Folha
+ * Generalização
+ * Especialização
+ * Tipos de Herança
+ * Classe Abstrata
+ * Metodo Abstrato
+ * Classe final
+ * Metodo final
+ */
+
+package a11Herança2;
+
+public class Principal11 {
+    public static void main(String[] args) {
+
+    }
+}
